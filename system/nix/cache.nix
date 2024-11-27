863bd9c5df9ff94bca33b62533d1ad0c152bf06e
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-11-27 17:13:11 +0600

    add nixpkgs-update cache

--- system/nix/cache.nix
+++ system/nix/cache.nix
@@ -8,13 +8,48 @@ _: {
       "https://nix-community.cachix.org"
       "https://nix-gaming.cachix.org"
       "https://cuda-maintainers.cachix.org"
+      "https://nixpkgs-update.cachix.org"
+
       # "https://hyprland.cachix.org"
+      # "https://nixpkgs-wayland.cachix.org"
+      # "https://helix.cachix.org"
+      # "https://catppuccin.cachix.org"
+      # "https://pwndbg.cachix.org"
+      # "https://numtide.cachix.org"
+      # "https://cache.garnix.io"
+      # "https://devenv.cachix.org"
+      # "https://cosmic.cachix.org"
+      # "https://emacsng.cachix.org"
+
+      # "https://iohk.cachix.org"
+      # "https://cache.iog.io"
+
+      # "https://cache.lix.systems"
+      # https://cache.floxdev.com"
     ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      "nixpkgs-update.cachix.org-1:6y6Z2JdoL3APdu6/+Iy8eZX2ajf09e4EE9SnxSML1W8="
+
       # "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      # "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+      # "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
+      # "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
+
+      # "pwndbg.cachix.org-1:HhtIpP7j73SnuzLgobqqa8LVTng5Qi36sQtNt79cD3k="
+      # "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+      # "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      # "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      # "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
+      # "emacsng.cachix.org-1:i7wOr4YpdRpWWtShI8bT6V7lOTnPeI7Ho6HaZegFWMI="
+
+      # "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
+      # "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+
+      # "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
+      # "flox-store-public-0:8c/B+kjIaQ+BloCmNkRUKwaVPFWkriSAd0JJvuDu4F0="
     ];
   };
 }
