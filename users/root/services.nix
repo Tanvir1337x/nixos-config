354de1faeb496ab29bf71c162715816f428a983e
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-11-27 23:12:55 +0600

    nginx: add flaresolverr instance

--- users/root/services.nix
+++ users/root/services.nix
@@ -218,8 +218,30 @@
               port = 80;
             }
           ];
+
           locations."/" = {
             proxyPass = "http://127.0.0.1:9117";
+          };
+
+          locations."/robots.txt" = {
+            extraConfig = ''
+              rewrite ^/(.*)  $1;
+              return 200 "User-agent: *\nDisallow: /";
+            '';
+          };
+        };
+
+        # Flaresolverr Instance
+        "flaresolverr.local" = {
+          listen = [
+            {
+              addr = "127.0.0.1";
+              port = 80;
+            }
+          ];
+
+          locations."/" = {
+            proxyPass = "http://127.0.0.1:8191";
           };
 
           locations."/robots.txt" = {
