f986033610f9f721eaf756dab5565ddf6b3a748a
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-12-05 21:09:41 +0600

    nginx: add netdata.local to virtual hosts

--- users/root/services.nix
+++ users/root/services.nix
@@ -287,6 +287,27 @@
             '';
           };
         };
+
+        # Netdata Instance
+        "netdata.local" = {
+          listen = [
+            {
+              addr = "127.0.0.1";
+              port = 80;
+            }
+          ];
+
+          locations."/" = {
+            proxyPass = "http://127.0.0.1:9822";
+          };
+
+          locations."/robots.txt" = {
+            extraConfig = ''
+              rewrite ^/(.*)  $1;
+              return 200 "User-agent: *\nDisallow: /";
+            '';
+          };
+        };
       };
     };
 
